f2fc9cb226e298916a28195ac0cb8476abbaae85
victpork <224617+victpork@users.noreply.github.com>
2026-02-11 15:00:49 +1300

    Modify User-Agent and add request headers
    Updated User-Agent header to include additional request headers for bypassing page checking

--- custom_components/auckland_bin_collection/sensor.py
+++ custom_components/auckland_bin_collection/sensor.py
@@ -22,8 +22,7 @@ _LOGGER = logging.getLogger(__name__)
 KEY_DATE = "date"
 KEY_TYPE = "type"
 URL_REQUEST = "https://www.aucklandcouncil.govt.nz/en/rubbish-recycling/rubbish-recycling-collections/rubbish-recycling-collection-days/"
-# Currently will work with any user-agent string
-UA_HEADER = "HA-Getter"
+UA_HEADER = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/144.0.0.0 Safari/537.36 Edg/144.0.0.0"
 
 def get_date_from_str(date_str: str) -> datetime.date:
     """Convert a date string to date object"""
@@ -49,7 +48,7 @@ async def async_get_bin_dates(hass: HomeAssistant, location_id: str):
     """Async method to get data from Auckland Council webpage."""
 
     url = f"{URL_REQUEST}{location_id}.html"
-    agent_header = {"User-Agent": UA_HEADER}
+    agent_header = {"User-Agent": UA_HEADER, "Accept-Encoding":"gzip, deflate, br, zstd", "Accept-Language": "en-NZ,en;q=0.9"}
     req_func = partial(requests.get, url, headers=agent_header)
     response = await hass.async_add_executor_job(req_func)
 
